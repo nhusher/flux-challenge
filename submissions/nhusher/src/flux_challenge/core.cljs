acae8414aa44ba90d4c2c0e3dd270ba85780ba3b
Nick Husher <nick.husher@faraday.io>
2015-10-27 15:00:26 -0400

    HACKING

--- submissions/nhusher/src/flux_challenge/core.cljs
+++ submissions/nhusher/src/flux_challenge/core.cljs
@@ -7,17 +7,9 @@
 
 (defonce app-state (atom {:active-planet nil
                           :ui-locked     true
-                          :sith-list     [{} {} {} {} {}]}))
+                          :sith-list     [nil nil nil nil nil]}))
 
-(defn from-planet? [planet sith] (= (-> sith :homeworld :id) (:id planet)))
-(defn relationship-to [a b]
-  (cond
-    (= (:id a) (:id b)) :identical
-    (= (-> a :master :id) (:id b)) :apprentice
-    (= (-> a :apprentice :id) (:id b)) :master
-    :else :no-relationship))
-
-(defn request-sith! [id cb]
+(defn get-sith [id cb]
   (let [handler (fn [res] (cb (with-meta res {::cancel (fn [])})))
         req (GET (str "http://localhost:3000/dark-jedis/" id) {:response-format :json
                                                                :keywords?       true
@@ -26,27 +18,26 @@
 
 (defn abort-sith-request! [sith] (if (-> sith meta ::cancel) ((-> sith meta ::cancel))))
 
-(defn handle-active-planet-change [app-state new-planet]
-  (let [planet-match (some (partial from-planet? new-planet) (:sith-list app-state))]
-    (when planet-match (doseq [sith (:sith-list app-state)] (abort-sith-request! sith)))
-    (assoc app-state :active-planet new-planet :ui-locked planet-match)))
+(defn from-planet? [planet sith] (= (-> sith :homeworld :id) (:id planet)))
 
-(defn insert-placeholder-sith [app-state sith]
-  (if (every? (partial = {}) (:sith-list app-state))
-    (assoc app-state :sith-list [{} {} sith {} {}])
-    app-state))
+(defn relationship-to [a b]
+  (cond
+    (= (:id a) (:id b)) :identical
+    (= (-> a :master :id) (:id b)) :apprentice
+    (= (-> a :apprentice :id) (:id b)) :master
+    :else :no-relationship))
 
-(defn insert-sith [app-state sith]
-  (assoc app-state :sith-list (map #(if (= (:id %) (:id sith)) sith %) (:sith-list app-state))))
+(defn get-sith-slot [sith sith-list]
+  (if (every? (partial = {}) sith-list)
+    0
+    (first (filter some? (map-indexed (fn [idx e] (case (relationship-to sith e)
+                                                    :master (dec idx)
+                                                    :apprentice (inc idx)
+                                                    :identical idx
+                                                    nil)) sith-list)))))
 
-(defn reducer [app-state {:keys [type payload]}]
-  (case type
-    :set-active-planet (handle-active-planet-change app-state payload)
-    :scroll-up (assoc app-state :sith-list (concat (take-last 3 (:sith-list app-state)) [{} {}]))
-    :scroll-down (assoc app-state :sith-list (concat [{} {}] (take 3 (:sith-list app-state))))
-    :sith-requested (insert-placeholder-sith app-state payload)
-    :sith-received (insert-sith app-state payload)
-    app-state))
+
+(declare reducer)
 
 (defn dispatch [action]
   (if (fn? action)
@@ -65,11 +56,46 @@
   (fn [dispatch]
     (dispatch (action :scroll-down))))
 
+(defn request-sith! [id]
+  (fn [dispatch]
+    (dispatch (action :sith-requested
+                      (get-sith id (fn [sith]
+                                     (let [sith-list (:sith-list app-state)
+                                           slot (get-sith-slot sith sith-list)]
+                                       (when (> slot 0) (request-sith! (:master sith)))
+                                       (when (< slot 4) (request-sith! (:apprentice sith))))
+                                     (dispatch (action :sith-received sith))))))))
+
 (defn boot! []
   (fn [dispatch]
     (dispatch (action :boot))
-    (dispatch (action :sith-requested
-                      (request-sith! 3616 #(dispatch (action :sith-received %)))))))
+    (request-sith! 3616)))
+
+(defn handle-active-planet-change [app-state new-planet]
+  (let [planet-match (some (partial from-planet? new-planet) (:sith-list app-state))]
+    (when planet-match (doseq [sith (:sith-list app-state)] (abort-sith-request! sith)))
+    (assoc app-state :active-planet new-planet :ui-locked planet-match)))
+
+(defn insert-placeholder-sith [app-state sith]
+  (let [sith-list (:sith-list app-state)
+        slot (get-sith-slot sith sith-list)]
+    (assoc app-state :sith-list (assoc slot sith-list sith))))
+
+(defn receive-sith [app-state sith]
+  (let [sith-list (:sith-list sith)
+        slot (get-sith-slot sith sith-list)]
+    (if (< -1 slot 4)
+      (assoc app-state :sith-list (assoc sith-list slot sith))
+      app-state)))
+
+(defn reducer [app-state {:keys [type payload]}]
+  (case type
+    :set-active-planet (handle-active-planet-change app-state payload)
+    :scroll-up (assoc app-state :sith-list (concat (take-last 3 (:sith-list app-state)) [{} {}]))
+    :scroll-down (assoc app-state :sith-list (concat [{} {}] (take 3 (:sith-list app-state))))
+    :sith-requested (insert-placeholder-sith app-state payload)
+    :sith-received (receive-sith app-state payload)
+    app-state))
 
 ;;
 ;; -- UI COMPONENTS ----------------
